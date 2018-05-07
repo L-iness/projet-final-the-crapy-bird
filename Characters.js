0b219facb5bd8b1fe763cee3c5ff3f960a59a551
L-iness <38474730+L-iness@users.noreply.github.com>
2018-05-07 17:00:23 +0200

    Ajout liste

--- Characters.js
+++ Characters.js
@@ -4,5 +4,13 @@ function Characters (Computer){
     var P3 = "Car";
     var P4 = "Class";
     var P5 = "Tourists";
+
+    var Psize = new Array ();
+    Psize[P1] = 1;
+    Psize[P2] = 2;
+    Psize[P3] = 3;
+    Psize[P4] = 4;
+    Psize[P5] = 5;
+
 }
 

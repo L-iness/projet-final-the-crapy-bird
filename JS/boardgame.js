6169668452b73b0c087b38828b9fbb25b690b6f0
tinou_99 <tinou99.f@gmail.com>
2018-05-21 15:45:29 +0200

    version bientôt finie

--- JS/boardgame.js
+++ JS/boardgame.js
@@ -2,13 +2,13 @@
 
 
 
-{generer les bateaux}
+{generer les bateaux} // verif
 
 
 {faire n fois :}
 		{demander une case}
-		verifier si le batteau est touché      {/détruit}
-		{l'afficher (web)}
+		verifier si le batteau est touché /détruit
+		l'afficher (web)
 
 
 --------X-------->
@@ -52,7 +52,7 @@ touche = [
 
 
 
-function generate(Char)
+function generer(Char)
 {
 	var C = [];
 			//generer une coordonnée et un sens
@@ -61,18 +61,18 @@ function generate(Char)
 			//10-19: colonne
 			var select = Math.floor(Math.random()*20);
 			// quand on a une ligne / col
-			// choisir un point de start
-			var start = Math.floor(Math.random()*(10-Char.size));
-		for(var i = 0; i < Bat.size; i++)
+			// choisir un point de debut
+			var debut = Math.floor(Math.random()*(10-Char.taille));
+		for(var i = 0; i < Char.taille; i++)
 		{
-			//suivre les coordonées "size" fois
+			//suivre les coordonées "taille" fois
 			if(select<10)
 			{
-				C.append([select,start+i]);
+				C.append([select,debut+i]);
 			}
 			else
 			{
-				C.append([start+i,select-10]);
+				C.append([debut+i,select-10]);
 			}
 		}
 		return C;
@@ -80,47 +80,99 @@ function generate(Char)
 
 
 
-function generateAll()
+function toutGenerer()
 {
-	var array=[];
-	for(var i = 0; i < charArray.length; i++)
+	var tableau=[];
+	for(var i = 0; i < persoTab.longueur; i++)
 	{
-		var coords = generer1(charArray[i]);
+		var coords = generer1(persoTab[i]);
 		// il faut tester si les coords se chevauchent et si oui recommencer
-		array.append(coords);
+		tableau.append(coords);
 	}
-	return array;
+	return tableau;
 }
 
 
 
-function verification(charArray,Coord)
+function verification(persoTab,Coord)
 {
-	for(var i = 0; i < charArray.length; i++)
+	for(var i = 0; i < persoTab.longueur; i++)
 	{
-		for(var j = 0; j < charArray[i].length; j++)
+		for(var j = 0; j < persoTab[i].longueur; j++)
 		{
-			if (charArray[i][j]==Coord)
+			if (persoTab[i][j]==Coord)
 			{
 				return [i,j];
 			}
 		}
 	}
 	return [1000,1000];
-} 
-
-function charArray(array)
-{
-	for(var i = 0; i < array.length; i++)
-	{
-		for(var j = 0; j < array[i].length; j++)
-		{
-			array[i][j] = true;
-		}
-	}
-	return array;
 }
 
+
+
+function persoTab(tableau)
+{
+	for(var i = 0; i < tableau.longueur; i++)
+	{
+		for(var j = 0; j < tableau[i].longueur; j++)
+		{
+			tableau[i][j] = true;
+		}
+	}
+	return tableau;
+}
+
+
+///////////////////////////////////////// IL FAUT QU'IL Y AIT id="tableau" dans le tableau de bataille navale
+function afficher(coords,bool)
+{
+	var x = coords[0];
+	var y = coords[1];
+	var cellnb = 1 + x + y*10;
+	var input = document.getElementById("tableau").innerHTML;
+	var tab = input.split("bgcolor=\"#"); // chaque case : "0000FF....."
+	if (bool)
+	{
+		tab[cellnb].replace("0000FF","00FF00");
+	}
+	else
+	{
+		tab[cellnb].replace("0000FF","FF0000");
+	}
+	var sortie = tab.join();  // on prend un tableau : ["a","b","c"] --> "abc"
+	document.getElementById("tableau").innerHTML = sortie;
+}
+
+function persoEnVie(tab)
+{
+	var S = "";
+	for(var i = 0; i < tab.longueur; i++)  // pour chaque bateau
+	{
+		var bool = false;
+		for(var j = 0; j < tab[i].longueur; j++)// on verifit si il est mort
+		{
+			if(tab[i][j]==true)
+			{
+				bool = true;
+			}
+		}
+		if(bool) // et on l'affiche
+		{
+			[S, persoTab[i].nom, " alive.\n"].join();
+		}
+		else
+		{
+			[S,persoTab[i].nom," dead.\n"].join();
+		}
+	}
+	alert(S);
+	// sortie : 
+	//		a alive.
+	//		b dead.
+	//		c dead.
+	//		d alive.
+}
 
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
@@ -128,21 +180,27 @@ function charArray(array)
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 
-var characters = generateAll();
-var booleens = charArray(characters);
+var persos = toutGenerer();
+var booleens = persoTab(persos);
+var nbcoups = 20;
 
 //...
 
-var coo = [3,5];
-var test = verifier(characters,coo);
-if(test != [1000,1000])
+while(nbcoups>0)
 {
-	booleens[test[0]][test[1]] = false;
-	alert("Yeah, on its face !");
+	var coo = [3,5];// a recuperer avec le js
+	var test = verifier(persos,coo);
+	if(test != [1000,1000])
+	{
+		booleens[test[0]][test[1]] = false;
+		afficher(coo,false);
+		alert("Yeah, on its face !");
+	}
+	else
+	{
+		alert("Oh ! Missed !");
+		afficher(coo,true);
+		nbcoups -= 1;
+	}
+	persoEnVie(booleens);
 }
-else
-{
-	alert("Oh ! Missed !"); 
-}
-
-

9b945ea7ce310549ed6b5bdc3401a6b98132c028
tinou_99 <tinou99.f@gmail.com>
2018-05-22 14:29:02 +0200

    petites modifs

--- JS/player.js
+++ JS/player.js
@@ -1,42 +1,42 @@
-function Player (){
-    this.name = "";
-    this.caracter = [];
+function Joueur (){
+    this.nom = "";
+    this.perso1 = [];
     this.nbessai = 0;
-    this.bird = [];
+    this.Type = [];
 }
 
-function NewPlayerName(Player){ 
+function NouveauJoueurNom(Joueur){ 
     var answer = false; //variable answer qui prend la valeur false
     while (answer == false){ //tant que answer est false
-        var name = prompt("Enter your name :"); //nouvelle variable name 
-        if (name == null){ //si name == null (rien)
+        var nom = prompt("Enter your nom :"); //nouvelle variable nom 
+        if (nom == null){ //si nom == null (rien)
             alert("Nope ..."); // on alerte l'utilisateur que sa réponse est incorrecte
             answer = false; //la réponse est donc false
         }
-        else if (name == ""){ //sinon si name est égale à autre chose
+        else if (nom == ""){ //sinon si nom est égale à autre chose
             alert("Nope ..."); // on alerte l'utilisateur que sa réponse est incorrecte
             answer = false; // la réponse est donc fausse
         }
         else{ 
-            Player.name = name; //le nom du joueur prend le nom qu'a rentré l'utilisateur
+            Joueur.nom = nom; //le nom du joueur prend le nom qu'a rentré l'utilisateur
         }
     }
 }
 
-function NewPlayerBird (Player) {
+function NouveauJoueurType (Joueur) {
     var answer2 = false;
-    if Player.name = name { //si le nom du joueur est correct
-        var bird = prompt("Enter your breed (pigeon or seagull) :") // création variable bird 
-        if (bird == "pigeon"){ //si bird == "pigeon"
+    if Joueur.nom = nom { //si le nom du joueur est correct
+        var Type = prompt("Enter your breed (pigeon or seagull) :") // création variable Type 
+        if (Type == "pigeon"){ //si Type == "pigeon"
             alert("Wow what a big pigeon you are !"); //on alerte le joueur que sa réponse a bien été prise en compte
             answer2 = true; //booléen : la réponse est vraie
         }
-        else if (bird == "seagull"){ // sinon si bird == "seagull"
-            alert("Fly little white bird ! Fly!"); // on alerte l'utilisateur que sa réponse a bien été prise en compte
+        else if (Type == "seagull"){ // sinon si Type == "seagull"
+            alert("Fly little white Type ! Fly!"); // on alerte l'utilisateur que sa réponse a bien été prise en compte
             answer2 = true; //booléen : la réponse est vraie
         }
         else { 
-            if (bird == null){ // si bird == null (rien)
+            if (Type == null){ // si Type == null (rien)
                 alert("Nope ..."); // on alert l'utilisateur que le nom n'est pas correct
                 answer2 = false; // la réponse est alors fausse
             }
@@ -48,9 +48,9 @@ function NewPlayerBird (Player) {
     }
 }
 
-function NewPlayer (Player){ 
+function NewJoueur (Joueur){ 
     if ((answer == true) && (answer2 == true)) { //si answer et answer2 sont vraies
-        alert("Welcome"+Player.name+"! You are a "+Player.bird+". As you know, you are at war with the Parisians who abuse you! Avenge yourself! Here we go!") //on alerte l'utilisateur
+        alert("Welcome"+Joueur.nom+"! You are a "+Joueur.Type+". As you know, you are at war with the Parisians who abuse you! Avenge yourself! Here we go!") //on alerte l'utilisateur
     }
     else {
         answer = false; //sinon on dit que answer et answer2 sont fausses et on recommence avec les deux fonctions ci-dessus
@@ -58,4 +58,4 @@ function NewPlayer (Player){
     }
 }
 
-Computer.character = CharacterStart(); 
+Computer.perso1 = Jeu(); 
--- JS/startgame.js
+++ JS/startgame.js
@@ -1,24 +1,24 @@
-function Game(){
-    var Player = new Player();
+function Jeu(){
+    var Joueur = new Joueur();
     var Computer;
-    NewPlayer(Player);
+    NewJoueur(Joueur);
 
-    alert(Player.name+" lands on a roof. He observes the moving life of Paris. Taken in hatred, he decides to launch an attack on these evil Parisians.");
+    alert(Joueur.name+" lands on a roof. He observes the moving life of Paris. Taken in hatred, he decides to launch an attack on these evil Parisians.");
     alert("He spotted in a park a Car parked on a pedestrian crossing in front of the park, a Parisian bobo and a Parisienne with a stroller strolling around, a class picnicing, and Tourists taking photos.")
-    alert("It's your turn to play "+Player.name+"! Blindly in the polluted sky of Paris, try to touch and scare off these evil Parisians!")
-    StartGame(Player, Computer);
+    alert("It's your turn to play "+Joueur.name+"! Blindly in the polluted sky of Paris, try to touch and scare off these evil Parisians!")
+    StartGame(Joueur, Computer);
 }
 
-function Winner(Player, Computer){
+function Gagnant(Joueur, Computer){
 	var totaltry = 20;
-    if (Player .nbessai > totaltry){
-        alert("You failed, "+Player.name+". The Parisians are stronger than you !");
+    if (Joueur.nbessai > totaltry){
+        alert("You failed, "+Joueur.name+". The Parisians are stronger than you !");
     }
-    else if(Player .nbessai < totaltry){
-        alert(+Player.name+", you win ! ")
+    else if(Joueur.nbessai < totaltry){
+        alert(+Joueur.name+", you win ! ")
     }
     else{
-        alert("Any winner today ... . "+Player.name+", try again later.");
+        alert("Any winner today ... . "+Joueur.name+", try again later.");
     }
     alert("Do you want to play again?")
 }
